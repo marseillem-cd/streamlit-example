b7b32e6f80780b45b5fcb7b474de63f6ab5814ad
marseillem-cd <78978661+marseillem-cd@users.noreply.github.com>
2023-01-04 14:46:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,25 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import snowflake.connector
+import pandas as pd
+from snowflake import connector
+import snowflake
+import numpy as np
+from st_aggrid import AgGrid
 
-"""
-# Welcome to Streamlit!
+#streamlit run C:\Users\marseille.ma\Streamlit\streamlit_SF.py
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+ctx = snowflake.connector.connect(
+    user="SVC_SPRING_DATASCIENCE",
+    password="silla!_lepidoptera682",
+    account="KYA28640",
+    warehouse="ANALYSTS_WH",
+    database="CD_ANALYTICS_TESTDB",
+    schema="ANALYTICSTESTDB_SCHEMA"
+)
+cur = ctx.cursor()
+cur.execute('select OPPID,CDUID,MODEL_PREDICTED_PROBABILITY  from "CD_ANALYTICS_TESTDB"."ANALYTICSTESTDB_SCHEMA"."SPRING_CLTV_PREDICTIONS" limit 100')
+df = cur.fetch_pandas_all()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("Spring Financial CLTV Scores")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+AgGrid(df)
