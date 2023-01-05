898667b14a43048b009433c21cce17cb79ee821d
marseillem-cd <78978661+marseillem-cd@users.noreply.github.com>
2023-01-04 16:25:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,42 @@ ctx = snowflake.connector.connect(
     schema="ANALYTICSTESTDB_SCHEMA"
 )
 cur = ctx.cursor()
-cur.execute('select OPPID,CDUID,MODEL_PREDICTED_PROBABILITY  from "CD_ANALYTICS_TESTDB"."ANALYTICSTESTDB_SCHEMA"."SPRING_CLTV_PREDICTIONS" limit 100')
+cur.execute('select OPPID,CDUID,MODEL_PREDICTED_PROBABILITY  from "CD_ANALYTICS_TESTDB"."ANALYTICSTESTDB_SCHEMA"."SPRING_CLTV_PREDICTIONS" limit 1000')
 df = cur.fetch_pandas_all()
 
-st.title("Spring Financial CLTV Scores")
+image=Image.open("C:/Users/marseille.ma/Streamlit/SpringFinancial.jpg")
+st.image(image,width=300)
 
-AgGrid(df)
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == 'spring':
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.header('_CLTV Scores_ :zap:')
+    st.caption('Please click the column to filter for the OPPID and CDUID you want!', unsafe_allow_html=False)
+    AgGrid(df)
